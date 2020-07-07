2ee789def941a07e15fa154b5a322dc8fa0d3055
AidayZaynutdinov <tfyhjmn@gmail.com>
2020-07-07 21:20:00 +0300

    add cloud-config and cloud-eureka examples

---
+++ cloud-eureka/lab-4-sentence/src/main/java/com/example/controllers/SentenceController.java
@@ -0,0 +1,40 @@
+package com.example.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class SentenceController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/sentence")
+    public @ResponseBody
+    String getSentence() {
+        return getWords("LAB-4-SUBJECT") + " "
+                + getWords("Lab-4-VERB") + " "
+                + getWords("Lab-4-ARTICLE") + " "
+                + getWords("Lab-4-ADJECTIVE") + " "
+                + getWords("Lab-4-NOUN") + ".";
+    }
+
+    public String getWords(String serviceName) {
+        List<ServiceInstance> services = discoveryClient.getInstances(serviceName);
+        if (services != null && services.size() > 0) {
+            URI uri = services.get(0).getUri();
+            if (uri != null) {
+                return (new RestTemplate()).getForObject(uri, String.class);
+            }
+        }
+        return null;
+    }
+}
